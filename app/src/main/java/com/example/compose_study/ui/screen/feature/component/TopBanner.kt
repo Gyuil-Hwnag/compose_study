e58ef123fb6c5b001c757a3b63977a36cec52332
duru.hi <hwanggyuil@gmail.com>
2023-05-23 16:38:54 +0900

    feat. 홈 > 탑 배너 인디케이터 추가

--- app/src/main/java/com/example/compose_study/ui/screen/feature/component/TopBanner.kt
+++ app/src/main/java/com/example/compose_study/ui/screen/feature/component/TopBanner.kt
@@ -4,12 +4,15 @@ package com.example.compose_study.ui.screen.feature.component
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -31,9 +34,13 @@ import kotlinx.coroutines.delay
 @ExperimentalPagerApi
 @Composable
 fun TopBannerScreen() {
-    val banner = TopBanner(imageUri = "https://www.howtodoandroid.com/images/coco.jpg", title = "헤어 스타일의 모든것\nBOBBED HAIR STYLE", description = "요즘 유행하는 스타일")
-    val banners = listOf<TopBanner>(banner, banner, banner, banner, banner, banner)
+    val banner1 = TopBanner(imageUri = "https://www.howtodoandroid.com/images/coco.jpg", title = "헤어 스타일의 모든것\nBOBBED HAIR STYLE", description = "요즘 유행하는 스타일")
+    val banner2 = TopBanner(imageUri = "https://cdn.travie.com/news/photo/first/201710/img_19975_1.jpg", title = "헤어 스타일의 모든것\nBOBBED NAIL STYLE", description = "요즘 유행하는 네일 스타일")
+    val banner3 = TopBanner(imageUri = "https://mblogthumb-phinf.pstatic.net/MjAxODA2MjZfMTMw/MDAxNTMwMDE0NDk2Mzcz.7re42MA5wqJxZlJ8J5FzfDKEEqugtVuhg49bSFYUuYsg.0Y0kjwH4oi1LXXpqrcGaVBch_4eQsyKyVTRsNtg7fCMg.JPEG.ichufs/%EC%82%AC%EC%A7%84%EC%8C%A4%EC%9A%B0%EC%93%B0%EB%9D%BC_3_0%EC%9D%B8%ED%8A%B8%EB%A1%9C.jpg?type=w800", title = "헤어 스타일의 모든것\nBOBBED ESTHETIC STYLE", description = "요즘 유행하는 에스테틱 스타일")
+
+    val banners = listOf<TopBanner>(banner1, banner2, banner3, banner1, banner2, banner3)
     val state = rememberPagerState()
+
     LaunchedEffect(key1 = state.currentPage) {
         delay(3000)
         var newPosition = state.currentPage + 1
@@ -47,47 +54,62 @@ fun TopBannerScreen() {
 
 @Composable
 fun TopBannerSlider(state: PagerState, banners: List<TopBanner>) {
-    HorizontalPager(
-        state = state,
-        count = banners.size, modifier = Modifier
-            .fillMaxWidth()
-    ) { page ->
-        TopBannerItem(banner = banners[page])
+    Box(contentAlignment = Alignment.BottomStart) {
+        HorizontalPager(
+            state = state,
+            count = banners.size, modifier = Modifier
+                .fillMaxWidth()
+        ) { page ->
+            TopBannerItem(banner = banners[page])
+        }
+
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 20.dp),
+                text = banners[state.currentPage].title,
+                color = Color.White,
+                fontSize = 22.sp
+            )
+
+            Row {
+                Text(
+                    modifier = Modifier
+                        .padding(top = 8.dp, start = 20.dp),
+                    text = banners[state.currentPage].description,
+                    color = Color.White,
+                    fontSize = 14.sp
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                BannerIndicator(current = state.currentPage + 1, totalCount = banners.size)
+            }
+            Spacer(modifier = Modifier.size(44.dp))
+        }
     }
 }
 
 @Composable
 fun TopBannerItem(banner: TopBanner) {
-    Box(contentAlignment = Alignment.BottomStart) {
-        AsyncImage(
-            model = banner.imageUri,
-            contentDescription = "배너 이미지",
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentHeight()
-                .aspectRatio(1f)
-        )
-        Column {
-            Text(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(start = 20.dp),
-                text = banner.title,
-                color = Color.White,
-                fontSize = 22.sp
-            )
+    AsyncImage(
+        model = banner.imageUri,
+        contentDescription = "배너 이미지",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .aspectRatio(1f)
+    )
+}
 
-            Text(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 8.dp, start = 20.dp),
-                text = banner.description,
-                color = Color.White,
-                fontSize = 14.sp
-            )
-            Spacer(modifier = Modifier.size(44.dp))
-        }
+@Composable
+fun BannerIndicator(current: Int, totalCount: Int) {
+    Surface(
+        modifier = Modifier.padding(5.dp),
+        shape = RoundedCornerShape(11.dp),
+        color = Color(0x4D111111)
+    ) {
+        Text(modifier = Modifier.padding(vertical = 4.dp, horizontal = 10.dp), text = "$current / $totalCount", fontSize = 11.sp, color = Color.White)
     }
 }
 
