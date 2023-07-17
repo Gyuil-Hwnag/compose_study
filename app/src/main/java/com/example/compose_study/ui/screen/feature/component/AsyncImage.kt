38c804d4b5bf9e776d164eeaec00515ef7ee06e2
duru.hi <hwanggyuil@gmail.com>
2023-07-17 19:24:12 +0900

    feat. coil-gif 공통 함수 추가

--- app/src/main/java/com/example/compose_study/ui/screen/feature/component/AsyncImage.kt
+++ app/src/main/java/com/example/compose_study/ui/screen/feature/component/AsyncImage.kt
@@ -10,6 +10,58 @@ import com.example.compose_study.R
 
 @Stable
 @Composable
+fun AsyncGifCrop(
+    modifier: Modifier,
+    imageUrl: String
+) {
+    AsyncImage(
+        modifier = modifier,
+        contentScale = ContentScale.Crop,
+        imageLoader = ImageLoader.Builder(LocalContext.current)
+            .components {
+                if (SDK_INT >= 28) add(ImageDecoderDecoder.Factory())
+                else add(GifDecoder.Factory())
+            }
+            .placeholder(R.drawable.shape_solid_n8_rectangle)
+            .error(R.drawable.shape_solid_n8_rectangle)
+            .build(),
+        model = ImageRequest.Builder(LocalContext.current)
+            .data(imageUrl)
+            .placeholder(R.drawable.shape_solid_n8_rectangle)
+            .error(R.drawable.shape_solid_n8_rectangle)
+            .build(),
+        contentDescription = "이미지"
+    )
+}
+
+@Stable
+@Composable
+fun AsyncGifFillBounds(
+    modifier: Modifier,
+    imageUrl: String
+) {
+    AsyncImage(
+        modifier = modifier,
+        contentScale = ContentScale.FillBounds,
+        imageLoader = ImageLoader.Builder(LocalContext.current)
+            .components {
+                if (SDK_INT >= 28) add(ImageDecoderDecoder.Factory())
+                else add(GifDecoder.Factory())
+            }
+            .placeholder(R.drawable.shape_solid_n8_rectangle)
+            .error(R.drawable.shape_solid_n8_rectangle)
+            .build(),
+        model = ImageRequest.Builder(LocalContext.current)
+            .data(imageUrl)
+            .placeholder(R.drawable.shape_solid_n8_rectangle)
+            .error(R.drawable.shape_solid_n8_rectangle)
+            .build(),
+        contentDescription = "이미지"
+    )
+}
+
+@Stable
+@Composable
 fun AsyncImageCrop(
     modifier: Modifier,
     imageUrl: String
