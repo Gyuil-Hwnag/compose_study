fb34a1c5fd88524caaa9898cc5c373bb45214d1c
duru.hi <hwanggyuil@gmail.com>
2023-07-17 19:25:25 +0900

    feat. coil-gif 공통 함수 추가

--- app/src/main/java/com/example/compose_study/ui/screen/feature/component/AsyncImage.kt
+++ app/src/main/java/com/example/compose_study/ui/screen/feature/component/AsyncImage.kt
@@ -1,10 +1,15 @@
 package com.example.compose_study.ui.screen.feature.component
 
+import android.os.Build.VERSION.SDK_INT
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import coil.ImageLoader
+import coil.compose.AsyncImage
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
 import coil.request.ImageRequest
 import com.example.compose_study.R
 
@@ -14,10 +19,11 @@ fun AsyncGifCrop(
     modifier: Modifier,
     imageUrl: String
 ) {
+    val context = LocalContext.current
     AsyncImage(
         modifier = modifier,
         contentScale = ContentScale.Crop,
-        imageLoader = ImageLoader.Builder(LocalContext.current)
+        imageLoader = ImageLoader.Builder(context)
             .components {
                 if (SDK_INT >= 28) add(ImageDecoderDecoder.Factory())
                 else add(GifDecoder.Factory())
@@ -25,7 +31,7 @@ fun AsyncGifCrop(
             .placeholder(R.drawable.shape_solid_n8_rectangle)
             .error(R.drawable.shape_solid_n8_rectangle)
             .build(),
-        model = ImageRequest.Builder(LocalContext.current)
+        model = ImageRequest.Builder(context)
             .data(imageUrl)
             .placeholder(R.drawable.shape_solid_n8_rectangle)
             .error(R.drawable.shape_solid_n8_rectangle)
@@ -66,7 +72,7 @@ fun AsyncImageCrop(
     modifier: Modifier,
     imageUrl: String
 ) {
-    coil.compose.AsyncImage(
+    AsyncImage(
         modifier = modifier,
         contentScale = ContentScale.Crop,
         model = ImageRequest.Builder(LocalContext.current)
@@ -84,7 +90,7 @@ fun AsyncImageFillBounds(
     modifier: Modifier,
     imageUrl: String
 ) {
-    coil.compose.AsyncImage(
+    AsyncImage(
         modifier = modifier,
         contentScale = ContentScale.FillBounds,
         model = ImageRequest.Builder(LocalContext.current)
