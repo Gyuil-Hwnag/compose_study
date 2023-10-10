bc1acba66ee0a4ac8442b000a322bad13252c920
Gyuil-Hwang <hwanggyuil@gmail.com>
2023-10-10 23:00:24 +0900

    feat. 갤러리 권한 OS13 확인

--- app/src/main/java/com/example/compose_study/ui/screen/permission/PermissionScreen.kt
+++ app/src/main/java/com/example/compose_study/ui/screen/permission/PermissionScreen.kt
@@ -2,13 +2,20 @@ package com.example.compose_study.ui.screen.permission
 
 import android.Manifest
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
 import android.os.Build
+import android.provider.MediaStore
+import android.provider.Settings
 import android.util.Log
 import androidx.activity.compose.ManagedActivityResultLauncher
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -17,30 +24,45 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
 @Composable
 fun PermissionScreen() {
     val context = LocalContext.current
     val cameraPermission = arrayOf(Manifest.permission.CAMERA)
-    val galleyPermission = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+    val galleyPermission = arrayOf(Manifest.permission.READ_MEDIA_IMAGES)
+
+    var imageUri by remember { mutableStateOf<Uri?>(null) }
+    val bitmap =  remember { mutableStateOf<Bitmap?>(null) }
+    val galleryLauncher = rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri: Uri? ->
+        imageUri = uri
+    }
 
     val launcherMultiplePermissions = rememberLauncherForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissionsMap ->
         val areGranted = permissionsMap.values.reduce { acc, next -> acc && next }
         if (areGranted) {
+            galleryLauncher.launch("image/*")
             Log.d("test", "권한이 동의되었습니다.")
         } else {
             Log.d("test", "권한이 거부되었습니다.")
+            openAppSettings(context)
         }
     }
     Scaffold(
@@ -55,25 +77,15 @@ fun PermissionScreen() {
         ) {
             Surface(
                 modifier = Modifier
-                    .background(color = Color.Blue)
-                    .padding(vertical = 12.dp, horizontal = 16.dp)
-                    .clickable {
-                        checkAndRequestPermissions(context = context, permissions = cameraPermission, launcher = launcherMultiplePermissions)
-                    }
-            ) {
-                Text(
-                    modifier = Modifier.background(color = Color.Blue),
-                    text = "Camera",
-                    color = Color.White
-                )
-            }
-            Spacer(modifier = Modifier.height(16.dp).wrapContentWidth())
-            Surface(
-                modifier = Modifier
                     .background(color = Color.Black)
                     .padding(vertical = 12.dp, horizontal = 16.dp)
                     .clickable {
-                        checkAndRequestPermissions(context = context, permissions = galleyPermission, launcher = launcherMultiplePermissions)
+                        checkAndRequestPermissions(
+                            context = context,
+                            permissions = galleyPermission,
+                            launcher = launcherMultiplePermissions,
+                            galleryLauncher = galleryLauncher
+                        )
                     }
             ) {
                 Text(
@@ -81,6 +93,26 @@ fun PermissionScreen() {
                     text = "Gallery",
                     color = Color.White
                 )
+            }
+
+            Spacer(modifier = Modifier.height(12.dp).wrapContentWidth())
+            imageUri?.let {
+                if (Build.VERSION.SDK_INT < 28) {
+                    bitmap.value = MediaStore.Images
+                        .Media.getBitmap(context.contentResolver,it)
+
+                } else {
+                    val source = ImageDecoder.createSource(context.contentResolver,it)
+                    bitmap.value = ImageDecoder.decodeBitmap(source)
+                }
+
+                bitmap.value?.let {  btm ->
+                    Image(
+                        bitmap = btm.asImageBitmap(),
+                        contentDescription = null,
+                        modifier = Modifier.size(250.dp)
+                    )
+                }
             }
         }
     }
@@ -90,14 +122,26 @@ fun checkAndRequestPermissions(
     context: Context,
     permissions: Array<String>,
     launcher: ManagedActivityResultLauncher<Array<String>, Map<String, Boolean>>,
+    galleryLauncher: ManagedActivityResultLauncher<String, Uri?>
 ) {
-
     if (permissions.all {
             ContextCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
         }) {
-        Log.d("test", "권한이 이미 존재합니다.")
+        galleryLauncher.launch("image/*")
     } else {
         launcher.launch(permissions)
         Log.d("test", "권한을 요청하였습니다.")
     }
 }
+
+fun openAppSettings(context: Context) {
+    val intent = Intent(
+        Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+        Uri.fromParts("package", context.packageName, null)
+    )
+    context.startActivity(intent)
+}
+
+
+
+
