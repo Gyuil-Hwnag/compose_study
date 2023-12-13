0eec2775d3c3aacfa33c6c2a5e7f50f04098b425
duru.hi <hwanggyuil@gmail.com>
2023-12-13 14:30:22 +0900

    feat. 스타일북 > Linear Progress Indicator 추가

--- app/src/main/java/com/example/compose_study/ui/screen/feature/component/StyleBook.kt
+++ app/src/main/java/com/example/compose_study/ui/screen/feature/component/StyleBook.kt
@@ -1,26 +1,36 @@
 package com.example.compose_study.ui.screen.feature.component
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.graphics.Color.Companion.LightGray
+import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
@@ -34,11 +44,13 @@ import kotlin.math.absoluteValue
 @Composable
 fun StyleBookScreen() {
     val pagerState = rememberPagerState(initialPage = styleBooks.infiniteLoopInitPage())
+    var pageOffset by remember { mutableStateOf(0f) }
+    val alphaOffset by remember { derivedStateOf { (1.3f) * pageOffset } }
 
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .background(Color.White)
+            .background(White)
     ) {
         StyleBookTitle()
         HorizontalPager(
@@ -47,16 +59,19 @@ fun StyleBookScreen() {
             modifier = Modifier.fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically
         ) { page ->
-            val pageOffset = pagerState.offsetForPage(page = page).absoluteValue
-            val alphaOffset = (1.3f) * pagerState.offsetForPage(page = page).absoluteValue
+            pageOffset = pagerState.offsetForPage(page = page).absoluteValue
             Card(
-                modifier = Modifier.fillMaxWidth().padding(horizontal = 12.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 12.dp),
                 shape = RoundedCornerShape(6.dp),
                 border = BorderStroke(1.dp, Color(0xFFEEEEEE)),
             ) {
                 StyleBookItem(item = styleBooks[page % styleBooks.size], pageOffset = pageOffset, alphaOffset = (1f - alphaOffset.coerceIn(0f, 1f)))
             }
         }
+        Spacer(modifier = Modifier.fillMaxWidth().size(12.dp))
+        StyleBookIndicator(pageOffset = pagerState.currentPageOffsetFraction, currentPage = (pagerState.currentPage % styleBooks.size) + 1, pageSize = styleBooks.size)
         Spacer(modifier = Modifier.fillMaxWidth().size(24.dp))
         ContentsDivider()
     }
@@ -101,7 +116,7 @@ fun StyleBookItem(item: StyleBook, pageOffset: Float, alphaOffset: Float) {
                     .fillMaxWidth()
                     .padding(vertical = 12.dp, horizontal = 20.dp),
                 text = item.title,
-                color = Color.White.copy(alpha = alphaOffset),
+                color = White.copy(alpha = alphaOffset),
                 fontSize = 18.sp
             )
             Text(
@@ -109,10 +124,28 @@ fun StyleBookItem(item: StyleBook, pageOffset: Float, alphaOffset: Float) {
                     .fillMaxWidth()
                     .padding(horizontal = 20.dp),
                 text = item.description,
-                color = Color.White.copy(alpha = alphaOffset),
+                color = White.copy(alpha = alphaOffset),
                 fontSize = 13.sp
             )
             Spacer(modifier = Modifier.size(24.dp))
         }
     }
+}
+
+@Composable
+fun StyleBookIndicator(pageOffset: Float, currentPage: Int, pageSize: Int) {
+    val animatedProgress = animateFloatAsState(
+        targetValue = (pageOffset + currentPage) / pageSize,
+        label = ""
+    ).value
+
+    LinearProgressIndicator(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(2.dp)
+            .padding(horizontal = 16.dp),
+        color = Black,
+        backgroundColor = LightGray,
+        progress = animatedProgress,
+    )
 }
