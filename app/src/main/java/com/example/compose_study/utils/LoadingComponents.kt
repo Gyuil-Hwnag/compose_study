0ad00359e53ae1537d92f06266d334887fb8628f
duru.hi <hwanggyuil@gmail.com>
2024-01-08 18:55:28 +0900

    feat. Compose > Shimmer 컴포넌트 추가

---
+++ app/src/main/java/com/example/compose_study/utils/LoadingComponents.kt
@@ -0,0 +1,61 @@
+package com.example.compose_study.utils
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LoadingFillMaxWidth(
+    ratio: Float,
+    color: Color = Color(0xFFEEEEEE),
+    isLoadingCompleted: Boolean
+) {
+    Box(
+        modifier = Modifier
+            .background(color = color)
+            .fillMaxWidth()
+            .aspectRatio(ratio)
+            .shimmerLoadingAnimation(isLoadingCompleted)
+    )
+}
+
+@Composable
+fun LoadingCornerRounded(
+    width: Int,
+    height: Int,
+    cornerRounded: Int,
+    color: Color = Color(0xFFEEEEEE),
+    isLoadingCompleted: Boolean
+) {
+    Box(
+        modifier = Modifier
+            .background(color = color, shape = RoundedCornerShape(size = cornerRounded.dp))
+            .width(width.dp)
+            .height(height.dp)
+            .shimmerLoadingAnimation(isLoadingCompleted)
+    )
+}
+
+@Composable
+fun LoadingCircleShape(
+    size: Int,
+    color: Color = Color(0xFFEEEEEE),
+    isLoadingCompleted: Boolean
+) {
+    Box(
+        modifier = Modifier
+            .background(color = color, shape = CircleShape)
+            .size(size.dp)
+            .shimmerLoadingAnimation(isLoadingCompleted)
+    )
+}
