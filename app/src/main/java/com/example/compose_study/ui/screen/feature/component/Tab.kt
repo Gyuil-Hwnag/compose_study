1a13864e60b8157276c6c63042c3a0b9993b28bb
Gyuil-Hwnag <hwanggyuil@gmail.com>
2024-01-11 23:16:21 +0900

    fix. 탭 관련 오류 수정

--- app/src/main/java/com/example/compose_study/ui/screen/feature/component/Tab.kt
+++ app/src/main/java/com/example/compose_study/ui/screen/feature/component/Tab.kt
@@ -2,17 +2,16 @@ package com.example.compose_study.ui.screen.feature.component
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material.Tab
 import androidx.compose.material.TabRowDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -23,11 +22,7 @@ fun Tab(
     onTabClicked: (index: Int, category: Category) -> Unit
 ) {
     val density = LocalDensity.current
-    val tabWidths = remember {
-        val tabWidthStateList = mutableStateListOf<Dp>()
-        repeat(tabs.size) { tabWidthStateList.add(0.dp) }
-        tabWidthStateList
-    }
+    var tabWidths by remember { mutableStateOf(List(tabs.size) { 0.dp }) }
 
     ScrollableTabRow(
         modifier = Modifier.padding(8.dp),
@@ -39,6 +34,7 @@ fun Tab(
                 modifier = Modifier.tabIndicatorOffset(
                     currentTabPosition = tabPositions[selectedTabIndex],
                     tabWidth = tabWidths[selectedTabIndex],
+//                    tabWidth = tabWidths.getOrNull(selectedTabIndex) ?: 0.dp
                 ),
                 color = Color.Black,
                 height = TabRowDefaults.IndicatorHeight * 0.5F
@@ -51,7 +47,10 @@ fun Tab(
                 modifier = Modifier.padding(8.dp).clickable { onTabClicked(index, tab) },
                 text = tab.name,
                 onTextLayout = { textLayoutResult ->
-                    tabWidths[index] = with(density) { textLayoutResult.size.width.toDp() }
+                    tabWidths = tabWidths.toMutableList().apply {
+                        if (index < size) this[index] = with(density) { textLayoutResult.size.width.toDp() }
+                        else add(with(density) { textLayoutResult.size.width.toDp() })
+                    }
                 },
                 color = Color.Black,
                 fontSize = 15.sp
