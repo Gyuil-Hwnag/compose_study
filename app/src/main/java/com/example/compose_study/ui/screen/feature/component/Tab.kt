35e17ddc0c21dd6ade9f68ad753171917bf5460c
Gyuil-Hwnag <hwanggyuil@gmail.com>
2024-01-11 23:27:27 +0900

    fix. 탭 리스트 변경시 오류 해결 코드 및 주석 추가

--- app/src/main/java/com/example/compose_study/ui/screen/feature/component/Tab.kt
+++ app/src/main/java/com/example/compose_study/ui/screen/feature/component/Tab.kt
@@ -15,6 +15,18 @@ import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+/**
+ * 에러 분석 설명
+ * 1. tabWidths의 경우 초기에 생성되는 만큼의 Tab Width를 가진 리스트가 remember 상태값을로 설정이 된다.
+ * 2. 현재의 탭 길이보다 긴 탭으로 바뀌어야 한다.
+ * 3. 현재 ScrollableTabRow가 forEachIndex를 돈다.
+ * 4. 그러나 tabWidths에 저장된 tab의 width 상태 리스트는 이전 tab을 기준으로 되어 있으므로, 현재탭 에서는 OutOfIndex가 발생할 수 밖에 없다.
+ *
+ * 해결 방법
+ * 1. tabWidths를 remember로 지정을 안하고 탭을 계속 새로 그린다? <- 채택X : 탭에 클릭 이벤트도 있는데, 그럼 탭이 클릭될 때도 매번 Composable을 그릴 것인가?
+ * 2. 기존 tab width 리스트의 사이즈를 넘어가는 탭에 대해서는 해당탭에서 분기처리를 해준다. <- 채택
+**/
+
 @Composable
 fun Tab(
     tabs: List<Category>,
@@ -22,7 +34,21 @@ fun Tab(
     onTabClicked: (index: Int, category: Category) -> Unit
 ) {
     val density = LocalDensity.current
+
+    /**
+     * Solve 코드
+     **/
     var tabWidths by remember { mutableStateOf(List(tabs.size) { 0.dp }) }
+
+    /**
+     * Error 코드
+     * 내용 비교하면 사실상 같은 코드인데 정리하고 안하고 차이
+     **/
+//    val tabWidths = remember {
+//        val tabWidthStateList = mutableStateListOf<Dp>()
+//        repeat(tabs.size) { tabWidthStateList.add(0.dp) }
+//        tabWidthStateList
+//    }
 
     ScrollableTabRow(
         modifier = Modifier.padding(8.dp),
@@ -34,7 +60,6 @@ fun Tab(
                 modifier = Modifier.tabIndicatorOffset(
                     currentTabPosition = tabPositions[selectedTabIndex],
                     tabWidth = tabWidths[selectedTabIndex],
-//                    tabWidth = tabWidths.getOrNull(selectedTabIndex) ?: 0.dp
                 ),
                 color = Color.Black,
                 height = TabRowDefaults.IndicatorHeight * 0.5F
@@ -47,10 +72,19 @@ fun Tab(
                 modifier = Modifier.padding(8.dp).clickable { onTabClicked(index, tab) },
                 text = tab.name,
                 onTextLayout = { textLayoutResult ->
+                    /**
+                     * Solve 코드
+                     **/
                     tabWidths = tabWidths.toMutableList().apply {
                         if (index < size) this[index] = with(density) { textLayoutResult.size.width.toDp() }
                         else add(with(density) { textLayoutResult.size.width.toDp() })
                     }
+
+                    /**
+                     * Error 코드
+                     * 현재 오류가 발생하는 IndexOutOfBounds가 발생하는 index >= size인 경우를 분기 처리하여 빈탭 Width를 만들어둔다
+                     **/
+//                    tabWidths[index] = with(density) { textLayoutResult.size.width.toDp() }
                 },
                 color = Color.Black,
                 fontSize = 15.sp
