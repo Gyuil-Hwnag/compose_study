82946d6b278023160d97c0ca91b4f05a01402222
duru.hi <hwanggyuil@gmail.com>
2024-04-14 15:09:39 +0900

    fix. 테스트 내용 추가

--- app/src/main/java/com/example/compose_study/utils/notification/LocalAlarmReceiver.kt
+++ app/src/main/java/com/example/compose_study/utils/notification/LocalAlarmReceiver.kt
@@ -5,6 +5,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import android.widget.Toast
 
 @Suppress("DEPRECATION")
 class LocalAlarmReceiver : BroadcastReceiver() {
@@ -13,6 +14,7 @@ class LocalAlarmReceiver : BroadcastReceiver() {
 
     @SuppressLint("UnsafeProtectedBroadcastReceiver")
     override fun onReceive(context: Context, intent: Intent) {
+        Toast.makeText(context, "Alarm Received", Toast.LENGTH_SHORT).show()
         firebaseMessagingService = FirebaseMessagingService(context = context)
         val pushMessage = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             intent.getParcelableExtra("PushMessage", PushMessage::class.java)
